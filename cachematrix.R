266242f55b6b668e0e519d588ae5c06389d458de
BradLipton <bradley.lipton@gmail.com>
2016-01-23 09:53:22 -0500

    here goes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions. The first makes a "matrix" object that stores a matrix and
+## has the ability to get the matrix, as well as cache an object passed to it. The
+## second function checks to see if the cache has an object in it. If so,
+## it delivers the cached object with a printed note. If not,
+## it calculates the inverse of the matrix from the object,  stores it in the cache.
+## and prints it.
 
-## Write a short comment describing this function
+## Function to store a matrix, get it, and cache an object passed to it.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mymatrix = matrix()) {
+      inverse <- NULL
+      get <- function() mymatrix
+      getinverse <- function() inverse
+      cacheinverse <- function(i) {
+            inverse <<- i
+      }
+      list(get=get, getinverse=getinverse, cacheinverse=cacheinverse)
 }
 
 
-## Write a short comment describing this function
+
+## Function to check the cache or calculate the inverse and cache it, delivering results
+## either way.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inverse <- x$getinverse()
+      if(!is.null(inverse)) {
+            message("getting cached data")
+            return(inverse)
+      }
+      thematrix <- x$get()
+      inverse <- solve(thematrix)
+      x$cacheinverse(inverse)
+      inverse
 }
